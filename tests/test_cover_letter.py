85006576d8c0b1bbe1047162df23b5c473492c6b
cerdamario13 <cerdamario13@gmail.com>
2025-12-06 15:08:50 -0600

    add word cover letter test

--- tests/test_cover_letter.py
+++ tests/test_cover_letter.py
@@ -58,3 +58,24 @@ def test_cover_letter_pdf():
         'data/test_data/output_files/cover_letter'
     )
 
+def test_cover_letter_word():
+    """
+    Test the generation of the pdf file
+    """
+
+    # Mocked cover_letter_data
+    cover_letter_data = {
+        "intro": "Dear Mr. Weyland,",
+        "paragraphs": {
+            "0": "I am excited to apply for the position of Science Officer aboard your exploratory deep space research vessel. With a Ph.D. in Astrobiology and Exobiology from Caltech, as well as a Master\'s degree in Astrophysics from the University of Cambridge, I have developed a strong foundation in planetary sciences and biological research in extreme environments. My over 15 years of experience in space exploration and exobiology, including leading biological research during a four-year mission to Saturn\\u2019s moon Titan with the European Space Agency, aligns well with the responsibilities of this role.",
+            "1": "Throughout my career, I have led scientific investigations focused on extraterrestrial sample collection, preservation, and analysis, ensuring rigorous safety protocols are maintained in hazardous conditions. I have coordinated closely with engineering and operations teams to integrate scientific findings into mission strategies, similar to the interdisciplinary collaboration required on your vessel. My experience in developing AI-assisted data pipelines to automate biological sample classification demonstrates my ability to leverage advanced technologies in remote environments. Additionally, my work at Weyland-Yutani as a Senior Research Scientist involved conducting microbial extremophile studies in simulated exoplanetary environments and establishing containment protocols to minimize environmental risks.",
+            "2": "I am confident that my background in astrobiology, experience in planetary survey protocols, and proven leadership in scientific research teams under constrained conditions will contribute significantly to the success of your mission. My extensive documentation of scientific findings and ability to communicate complex data clearly to diverse audiences will support Weyland-Yutani\'s objectives of building better worlds through pioneering exploration."
+        }
+    }
+
+    # Build the file
+    resi.cover_letter.build_cover_letter_word(
+        cover_letter_data,
+        'data/test_data/user_history.json',
+        'data/test_data/output_files/cover_letter'
+    )
